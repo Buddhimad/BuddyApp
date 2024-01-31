843d2805f297289d00273002750bae36bfb8b63d
Buddhima Dahanayake <b.v.dahanayake0812@gmail.com>
2024-02-01 01:48:18 +0530

    add Filter to pharmacy Dashboard
    add Filter to pharmacy Dashboard

--- BuddyApp/src/app/Pages/pharmacy-dashboard/pharmacy-dashboard.component.ts
+++ BuddyApp/src/app/Pages/pharmacy-dashboard/pharmacy-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatMenuModule} from '@angular/material/menu';
@@ -8,17 +9,23 @@ import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { SharedService } from '../../Services/shared-service.service';
 import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {JsonPipe} from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {NativeDateAdapter} from '@angular/material/core';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-pharmacy-dashboard',
   standalone: true,
-  imports: [CommonModule,MatIconModule,MatTooltipModule,MatMenuModule,MatChipsModule,MatInputModule,MatFormFieldModule,NgxMaterialTimepickerModule],
+  providers: [NativeDateAdapter],
+  imports: [CommonModule,MatIconModule,MatTooltipModule,MatMenuModule,MatChipsModule,MatInputModule,MatFormFieldModule,NgxMaterialTimepickerModule,MatDatepickerModule,JsonPipe,MatNativeDateModule,FormsModule, ReactiveFormsModule],
   templateUrl: './pharmacy-dashboard.component.html',
   styleUrl: './pharmacy-dashboard.component.css'
 })
 export class PharmacyDashboardComponent {
+  value = '';
   private subscription: Subscription;
   constructor(private router: Router, private sharedService: SharedService) {
     this.subscription = this.sharedService.routeControlFunction.subscribe((route:any)=>{
@@ -29,4 +36,8 @@ export class PharmacyDashboardComponent {
    navigateToDestination(destination:String) {
     this.router.navigate([destination]);
   }
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 }
